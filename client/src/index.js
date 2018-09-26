00c9094b187acc055b4772eab6ec4d0c778602b4
Secure Cap <Twilliams397tw@gmail.com>
2018-09-25 21:02:41 -0500

    added scripts

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import {render} from 'react-dom';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {
+    return <div>
+     <h1>React App Running!</h1>
+    </div>
+   }
+   
+   render(
+    <App/>,
+    document.getElementById('app')
+   )
+
+ReactDOM.render(<App />, document.getElementById('app'));
 registerServiceWorker();
+
+
